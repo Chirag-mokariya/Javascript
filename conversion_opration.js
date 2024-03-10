afd2435817b7d2f0e86d9910fc92266efb2127c4
chirag-mokariya <mokariyachirag10@gmail.com>
2024-03-10 23:59:27 +0530

    conversion opration in js

---
+++ conversion_opration.js
@@ -0,0 +1,16 @@
+let score="chirag"
+console.log(typeof score)
+console.log(score)
+
+let print=Number(score)   // also convert in string , boolean, array, object,ect...
+console.log(print)
+
+/* ---------------------------------------- */
+
+let isLoggedIn=0
+
+let conversion=Boolean(isLoggedIn)
+console.log(conversion)
+
+
+

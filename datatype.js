c11433e1dc1027ea6ff18a668f5b82284653e970
chirag-mokariya <mokariyachirag10@gmail.com>
2024-03-10 23:58:06 +0530

    add new js files

---
+++ datatype.js
@@ -0,0 +1,26 @@
+let age=19
+let Name="chirag"
+let accountname='SBI'
+let isLoggedIn=true
+let accType;
+let report=null
+
+/*
+*******types of datatype:*********
+number
+bigint
+string  // use signle or double quate both are valid
+boolean
+undefined
+null // standalone value
+symbol // unique
+object
+
+*/
+console.log(typeof age);
+console.log(typeof Name);
+console.log(typeof accountname);
+console.log(typeof isLoggedIn);
+console.log(typeof accType);
+console.log(typeof report);
+
---
+++ variable.js
@@ -0,0 +1,11 @@
+const accountId = 1001
+let accountName= "SBI"
+var accountEmail="sbi@gmail.com"
+
+
+// accountId=101       // not allowed to change const variable
+accountName="kotak"
+accountEmail="kotak@gmail.com"
+
+console.log(accountId);
+console.table([accountId,accountName,accountEmail])
